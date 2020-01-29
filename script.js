e967b6a07c285cce275d661f7597f7ffe53185da
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2020-01-29 17:25:44 +0100

    Damier en cours

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-var $;
 (function () {
    $(document).ready(function () {
-        console.log("dom pret");
-        $(".p").fadeIn(5000);
-        $(".i").fadeIn(500);
+       console.log("dom pret");
+       $('.p').fadeIn(500);
+       $('.i').fadeIn(500);
+
+       $('.p').append(
+           $('.i').css('display', 'flex')
+       )
+       $('.i').fadeIn(500);
+       /*
+       for(i=0;i < 10; ++i) {
+            $(".p").fadeOut(50);
+            $(".i").fadeOut(50);
+            $(".p").fadeIn(50);
+            $(".i").fadeIn(50);
+       }
+       */
+
+
+
    });
 })();
