ba1045a37eb02a06ec5b2c7778b3a1d01de4fcdb
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2020-01-31 11:08:31 +0100

    damier en bois centré avec bordure

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 (function () {
-   $(document).ready(function () {
-       console.log("dom pret");
+    $(document).ready(function () {
+        console.log("dom pret");
 
-       for (l=0;l<10;++l){
-           if (l%2 == 0){
-               for (c = 0; c < 10; ++c){
-                   if (c%2 == 0) {
-                       $("#damier").append('<div class="i" />');
-                   } else {
-                       $("#damier").append('<div class="p" />');
-                   }
-               }
-           } else {
-               for (c = 0; c < 10; ++c){
-                   if (c%2 == 0) {
-                       $("#damier").append('<div class="p" />');
-                   } else {
-                       $("#damier").append('<div class="i" />');
-                   }
-               }
-           }
-       }
+        for (l=0;l<10;++l){
+            if (l%2 == 0){
+                for (c = 0; c < 10; ++c){
+                    if (c%2 == 0) {
+                        $("#damier").append('<div class="i" />');
+                    } else {
+                        $("#damier").append('<div class="p" />');
+                    }
+                }
+            } else {
+                for (c = 0; c < 10; ++c){
+                    if (c%2 == 0) {
+                        $("#damier").append('<div class="p" />');
+                    } else {
+                        $("#damier").append('<div class="i" />');
+                    }
+                }
+            }
+        }
+        $('#damier').fadeIn(800).css('display', 'flex');
+        $('.p').fadeIn(800);
+        $('.i').fadeIn(800);
 
-       $('.p').fadeIn(800);
-       $('.i').fadeIn(800);
 
-
-       /* clignotement */
-       for(i=0;i < 10; ++i) {
+        /* clignotement
+        for(i=0;i < 10; ++i) {
             $('.p').fadeOut(250);
             $('.i').fadeOut(250);
+            $('#damier').fadeOut(250);
             $('.p').fadeIn(250);
             $('.i').fadeIn(250);
-       }
+            $('#damier').fadeIn(250).css('display', 'flex');
+        }*/
 
-   });
+    });
 })();
