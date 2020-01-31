0b492c4d89db1949bfee5836161c6df82190231c
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2020-01-31 13:48:03 +0100

    input number regle taille

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//var nbLigne = document.getElementById("nbLigne");
 
 function aparaitre() {
     $('#damier').fadeIn(800).css('display', 'flex');
@@ -22,41 +23,35 @@ function clignoter() {
     }
 }
 
-(function () {
-    $(document).ready(function () {
-        console.log("dom pret");
-
-        for (l=0;l<10;++l){
-            if (l%2 == 0){
-                for (c = 0; c < 10; ++c){
-                    if (c%2 == 0) {
-                        $("#damier").append('<div class="i" />');
-                    } else {
-                        $("#damier").append('<div class="p" />');
-                    }
+function construire(nbLigne){
+    $('#damier div').remove();
+    let nbCase = nbLigne*50;
+    $('#damier').css('width',nbCase.toString()+'px');
+    for (l=0;l<nbLigne;++l){
+        if (l%2 == 0){
+            for (c = 0; c < nbLigne; ++c){
+                if (c%2 == 0) {
+                    $("#damier").append('<div class="i" />');
+                } else {
+                    $("#damier").append('<div class="p" />');
                 }
-            } else {
-                for (c = 0; c < 10; ++c){
-                    if (c%2 == 0) {
-                        $("#damier").append('<div class="p" />');
-                    } else {
-                        $("#damier").append('<div class="i" />');
-                    }
+            }
+        } else {
+            for (c = 0; c < nbLigne; ++c){
+                if (c%2 == 0) {
+                    $("#damier").append('<div class="p" />');
+                } else {
+                    $("#damier").append('<div class="i" />');
                 }
             }
         }
+    }
+    aparaitre();
+}
 
-
-
-        /* clignotement
-        for(i=0;i < 10; ++i) {
-            $('.p').fadeOut(250);
-            $('.i').fadeOut(250);
-            $('#damier').fadeOut(250);
-            $('.p').fadeIn(250);
-            $('.i').fadeIn(250);
-            $('#damier').fadeIn(250).css('display', 'flex');
-        }*/
-
+(function () {
+    $(document).ready(function () {
+        console.log("dom pret");
+      //  console.log(nbLigne);
     });
 })();
