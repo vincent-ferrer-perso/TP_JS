076621806a0059c8df53525a12dfa81c1df283f2
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2020-02-05 17:24:04 +0100

    damier X O

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //var nbLigne = document.getElementById("nbLigne");
-
+/*
 function aparaitre() {
     $('#damier').fadeIn(800).css('display', 'flex');
     $('.p').fadeIn(800);
@@ -53,5 +53,106 @@ function construire(nbLigne){
     $(document).ready(function () {
         console.log("dom pret");
       //  console.log(nbLigne);
+
+        let css;
+        $('#damier div').hover(function(){
+            css = $(this).css("background-color")
+            $(this).css("background-color", "blue");
+        }, function(){
+            $(this).css("background-color", css);
+        });
+
     });
+})();
+*/
+/* correction damier */
+
+class Damier {
+
+    constructor(id, ligne) {
+        this.ligne = ligne;
+
+        let joueurEnCours = 1;
+
+        let css_cell = {
+            'display': 'inline-block',
+            'min-width': '30px',
+            'min-height': '30px',
+            'margin': '0',
+            'padding': '0',
+            'text-align':'center',
+            'line-height':'30px'
+
+        };
+
+
+        let css_white = {
+            'background-color': 'white',
+            'color': 'black'
+        };
+
+        let css_black = {
+            'background-color': 'black',
+            'color': 'white'
+        };
+
+        let tab = [];
+
+        let css_damier = {
+            'width': ligne*30,
+            'border':'solid green 10px',
+            'margin-left':'auto',
+            'margin-right':'auto'
+        }
+
+        $('#damier').css(css_damier);
+        for (let y=0; y<ligne; y++) {
+            let line = $('<div></div>');
+            for(let x=0; x<ligne;x++){
+                let cell = $('<div>&nbsp;</div>').css(css_cell);
+                cell.css((x + y) % 2 ? css_white : css_black);
+                cell.data('x',x);
+                cell.data('x',y);
+                cell.data('statut',false)
+                line.append(cell);
+                let old_css;
+                cell.hover(function(){
+                    old_css = $(this).css("background-color")
+                    $(this).css("background-color", "brown");
+                }, function(){
+                    $(this).css("background-color", old_css);
+                });
+
+                cell.click(function() {
+                    if(!$(this).data('statut')) {
+                        if (joueurEnCours == 1) {
+                            $(this).html('X');
+                            joueurEnCours = 2;
+                            console.log("J1 a joué");
+                            $('#joueurEnCours').html('Le tour de J2');
+                        } else {
+                            $(this).html('0');
+                            joueurEnCours = 1;
+                            console.log("J2 a joué");
+                            $('#joueurEnCours').html('Le tour de J1');
+                        }
+                        $(this).data('statut',true);
+                    } else {
+                        $('#joueurEnCours').append(" Case impossible");
+                    }
+                });
+
+
+            }
+            $(id).append(line);
+        }
+    }
+}
+
+
+
+(function() {
+    $(document).ready( function(){
+        new Damier('#damier', 20);
+    })
 })();
